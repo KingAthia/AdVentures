2e3987677b78249228229b9208a0928cb50f9b01
ayubsoft254 <henryoayub15@gmail.com>
2024-09-17 15:35:49 +0300

    Refactor Ad model field names for consistency; update open_slots calculation in AdCategory

--- src/ads/models.py
+++ src/ads/models.py
@@ -2,33 +2,33 @@ from django.db import models
 from django.contrib.auth.models import User
 
 CATEGORIES = [
-    ('BILLBOARDS','BILLBOARDS'),
-    ('TAXI','TAXI'),
-    ('WEBSITE','WEBSITE'),
-    ('CAMPAIGNS','CAMPAIGNS'),
+    ('BILLBOARDS', 'BILLBOARDS'),
+    ('TAXI', 'TAXI'),
+    ('WEBSITE', 'WEBSITE'),
+    ('CAMPAIGNS', 'CAMPAIGNS'),
 ]
 
 class AdCategory(models.Model):
     name = models.CharField(max_length=20, choices=CATEGORIES, unique=True)
     max_slots = models.PositiveIntegerField(default=10)  # Set default max slots
-    price = models.DecimalField(max_digits=6, decimal_places=2)    
-    
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
     @property
     def open_slots(self):
         # Calculate open slots dynamically
         booked_slots = self.ads.count()
-        return self.total_slots - booked_slots
+        return self.max_slots - booked_slots
 
     def __str__(self):
         return self.name
+
 class Ad(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='staticfiles/ads/')
     description = models.TextField()
-    adCategory = models.ForeignKey(AdCategory, on_delete=models.CASCADE, related_name='ads')
-    created_at = models.DateTimeField(auto_now_add=True)    
-    
-    def __str__(self):
-        return self.title  
+    ad_category = models.ForeignKey(AdCategory, on_delete=models.CASCADE, related_name='ads')
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title

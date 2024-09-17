d6618b668943cf5fd7ba62b8650731199daa5b30
ayubsoft254 <henryoayub15@gmail.com>
2024-09-17 15:06:42 +0300

    Add AdCategory model and update Ad model to use foreign key

--- src/ads/models.py
+++ src/ads/models.py
@@ -8,14 +8,26 @@ CATEGORIES = [
     ('CAMPAIGNS','CAMPAIGNS'),
 ]
 
-# Create your models here.
+class AdCategory(models.Model):
+    name = models.CharField(max_length=20, choices=CATEGORIES, unique=True)
+    max_slots = models.PositiveIntegerField(default=10)  # Set default max slots
+    
+    @property
+    def open_slots(self):
+        # Calculate open slots dynamically
+        booked_slots = self.ads.count()
+        return self.total_slots - booked_slots
+
+    def __str__(self):
+        return self.name
 class Ad(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ads')
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='staticfiles/ads/')
     description = models.TextField()
-    adCategory = models.CharField(max_length=20,choices=CATEGORIES)
+    adCategory = models.ForeignKey(AdCategory, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)    
     
     def __str__(self):
-        return self.title
+        return self.title  
+

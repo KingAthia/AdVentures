54a4ee8f265e5a6c461d9f008fa0b56058d92959
ayubsoft254 <henryoayub15@gmail.com>
2024-09-19 00:10:20 +0300

    Add payment verification URL and link in payment summary template

--- src/ads/urls.py
+++ src/ads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from ads.views import home, create_ad, ad_payment
+from ads.views import home, create_ad, ad_payment, payment_verification
 
 urlpatterns = [
     path('', home, name='home'),
     path('book', create_ad, name='create_ad'),
-    path('ad_payment/', ad_payment, name='ad_payment'),    
+    path('ad_payment/', ad_payment, name='ad_payment'),
+    path('payment_verification', payment_verification, name='payment_verification'),    
 ]
